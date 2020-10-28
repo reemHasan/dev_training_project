311b3980ea14649f8f5abcb4eb0ad45f8508d676
reemHasan <reem.mathbout89@yahoo.com>
2020-10-29 00:07:44 +0100

    add form

--- currency_converter/src/component/converter/converter.js
+++ currency_converter/src/component/converter/converter.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import converter from "./converter.css";
 import { Button, Container } from 'reactstrap'
+import Select from 'react-select'
+import {FormControl,Form,Col} from 'react-bootstrap'
 
 class Converter extends React.Component {
   constructor(props) {
@@ -23,7 +25,7 @@ class Converter extends React.Component {
       .then(response => {
         const currencyAr = ["EUR"];
         for (const key in response.data.rates) {
-          currencyAr.push(key);
+          currencyAr.push({value:key});
         }
         this.setState({ currencies: currencyAr });
       })
@@ -75,51 +77,72 @@ class Converter extends React.Component {
     }
   };
 
+
   render() {
     return (
-      <Container style={{ marginTop: 20 }}>
-      <div className="Converter">
-        <h2>
-          <span>Currency</span>Converter
-          <span role="img" aria-label="money">
+      <div class="container h-100">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-10 col-md-8 col-lg-6">
+        <Form>
+          <div classNmae="title">
+           <h2>
+            <span>Currency</span>Converter
+            <span role="img" aria-label="money">
             &#x1f4b5;
-          </span>
-        </h2>
-        <div className="Form">
-          <input
-            name="amount"
-            type="text"
-            value={this.state.amount}
-            onChange={event => this.setState({ amount: event.target.value })}
-          />
-          <select
-            name="from"
-            onChange={event => this.selectHandler(event)}
-            value={this.state.fromCurrency}
-          >
-            {this.state.currencies.map(cur => (
-              <option key={cur}>{cur}</option>
-            ))}
-          </select>
-          <select
-            name="to"
-            onChange={event => this.selectHandler(event)}
-            value={this.state.toCurrency}
-          >
-            {this.state.currencies.map(cur => (
-              <option key={cur}>{cur}</option>
-            ))}
-          </select>
-          <Button onClick={this.convertHandler}>Convert</Button>
-          <div>
-          <span> </span>
-          {this.state.result && <h5> 1 {this.state.fromCurrency} is equal to {this.state.oneUnit} {this.state.toCurrency}</h5>}
-          <br />
-          {this.state.result && <h3>The result of converted amount is <span>{this.state.result}</span></h3>}
+            </span>
+           </h2>
           </div>
+          <br/>
+         <Form.Group controlId="exampleForm.ControlInput1">
+           <Form.Label>Amount </Form.Label>
+           <Form.Control type="text" placeholder="Enter amount here"          
+           name="amount"
+          onChange={event => this.setState({ amount: event.target.value })}/>
+         </Form.Group>
+
+         <Form.Group controlId="exampleForm.ControlSelect1">
+           <Form.Label >Source Currency</Form.Label>
+           <Form.Control as="select" name="from" 
+           onChange={event => this.selectHandler(event)}
+           value={this.state.fromCurrency}
+           >
+           {
+               this.state.currencies.map((option, index) => {
+                return (<option key={index} value={option.value}>{option.value}</option>)
+                  })
+                 }
+           </Form.Control>
+         </Form.Group>
+
+         <Form.Group controlId="exampleForm.ControlSelect2">
+           <Form.Label >Distination Currency</Form.Label>
+           <Form.Control as="select" name='to'
+           onChange={event => this.selectHandler(event)}
+           value={this.state.toCurrency}
+           >
+           {
+               this.state.currencies.map((option, index) => {
+                return (<option key={index} value={option.value}>{option.value}</option>)
+                  })
+                 }
+           </Form.Control>
+         </Form.Group>
+         <div>
+         <Button className="button" onClick={this.convertHandler}>Convert</Button>
+           <br/>
+           <br/>
+         {this.state.result && <div className="divConvert">
+          <h5> 1 {this.state.fromCurrency} is equal to {this.state.oneUnit} {this.state.toCurrency}</h5>
+          <br />
+          <h3>The result of converted amount is <span>{this.state.result}</span></h3>
+         </div>}
         </div>
-      </div>
-      </Container>
+      </Form>
+      {/*Form end */}
+        </div>
+    </div>
+</div>
+
     );
   }
 }
