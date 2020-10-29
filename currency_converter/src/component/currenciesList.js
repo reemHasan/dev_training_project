51ce389a89dfc52e78db7692a833708c75dad15d
reemHasan <reem.mathbout89@yahoo.com>
2020-10-30 00:10:44 +0100

    update

--- currency_converter/src/component/currenciesList.js
+++ currency_converter/src/component/currenciesList.js
@@ -1,7 +1,10 @@
 
 import React from "react";
 import myJson from '../data/currencies_details.json';
+import Image from 'react-bootstrap/Image';
 import * as reactBootStrap from 'react-bootstrap';
+import TableScrollbar from 'react-table-scrollbar';
+import "./currenciesList.css"
 //import Link from '../topbar-link/topbar-link';
 
 class CurrenciesList extends React.Component {
@@ -9,45 +12,63 @@ class CurrenciesList extends React.Component {
       super();
       this.state = {
        currencyList:[],
+       image_source: '',
       }
     }
 
     componentDidMount() {
         this.setState({currencyList: myJson})
-      };
+    }
+    
     
     render () {
+
+       // var image=require('./ad.png');
         const rows = this.state.currencyList.map((currency, index) =>{
-            return (<tr>
-                <td>{currency.Country_Name}</td>
+            //this.setState({ image_source: currency.flag_source });
+            return (
+            <tr>
+                {/*<td><Image src={require(this.state.image_source)}/></td>*/}
+                <td><Image src={require('./images/fr.png')}/></td>
+                <td>{currency.Country_Name.toString('utf8')}</td>
                 <td>{currency.Currency_Name}</td>
-                <td>{currency.Currency_Symbol}</td>
+                <td>{currency.Currency_Symbol.toString('utf8')}</td>
                 <td>{currency.ISO_code}</td>
                 <td>{currency.Fractional_unit}</td>
+                {/*console.log({currency.flag_source});*/}
               </tr>)
-        });
 
-        return (
-          /* <table>
-           <tbody>
-               {rows}
-           </tbody>
-           </table>*/
-        //console.log("Country_Name", this.state.currencyList)
-        <reactBootStrap.Table responsive striped bordered hover variant="dark">
-           <thead>
+        });
+    console.log(this.state.currencyList)
+      return (
+            
+      <div class="container">
+          <div>
+              <h1>List of supported currencies and thier coutries</h1>
+          </div>
+        <div class="row">
+        <div class="col-10">
+            {/*class='flags-table flex-vertical'*/}
+            <TableScrollbar>
+            <reactBootStrap.Table responsive striped bordered hover variant="dark" className='mt-5'>
+           <thead >
             <tr>
+            <th>Country Flag</th>   
              <th>Country Name</th>
              <th>Currency Name</th>
              <th>Currency Symbol</th>
              <th>ISO Code</th>
              <th>Fractional Unit</th>
             </tr>
-           </thead>
-          <tbody>
-           {rows}
-          </tbody>
-        </reactBootStrap.Table>
+            </thead>
+            <tbody>
+             {rows}
+           </tbody>
+           </reactBootStrap.Table>
+          </TableScrollbar>
+          </div>
+         </div>
+        </div>
         )
 
     }
