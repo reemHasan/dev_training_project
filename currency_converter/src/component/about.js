6da7758b7af774dd8ff84d1e58588a6be209d7b7
reemHasan <reem.mathbout89@yahoo.com>
2020-10-30 00:08:56 +0100

    add about details

--- currency_converter/src/component/about.js
+++ currency_converter/src/component/about.js
@@ -1,9 +1,24 @@
 
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import "./about.css"
 const About = () => {
   return (
-    <div>
-      <h3>About !!!!!!</h3>
+    <div class="container">
+    <div class="row">
+        <div class="col-10">
+      <Card className="text-center">
+      <Card.Header className='title'>About the webSite</Card.Header>
+     <Card.Body >
+    <Card.Title>Developped by: Reem MATHBOUT and Pierre EPRON </Card.Title>
+    <Card.Text>
+      The goal of this website is to discover the basics of web delopping techniques.
+    </Card.Text>
+  </Card.Body>
+  <Card.Footer className="text-muted">Ecole IA Simplone 2020-2021</Card.Footer>
+</Card>
+    </div>
+    </div>
     </div>
   );
 }
