48ddbe31f24c833404a75fdbe3e5679903a76d42
reemHasan <reem.mathbout89@yahoo.com>
2020-10-29 11:31:46 +0100

    modify links

--- currency_converter/src/component/navBar.js
+++ currency_converter/src/component/navBar.js
@@ -3,21 +3,21 @@ import React, { Component } from "react";
 import {FormControl,NavDropdown,Navbar,Nav,Form,Button} from 'react-bootstrap'
 
 class NavigatBar extends Component {
-    render() {
-      return (
-      <div>
-    <Navbar bg="dark" variant="dark">
-  <Navbar.Brand href="#home">Currency Conveter</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Currencies</Nav.Link>
-      <Nav.Link href="#link">About</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-    </div>
+render() {
+return (
+<div>
+<Navbar bg="dark" variant="dark">
+    <Navbar.Brand to="/">CurrencyConveter</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/currenciesList">Currencies</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+</div>
     );
     }
   }
