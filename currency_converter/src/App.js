a281b991f0975b12100dc8292f30b190cccc4721
reemHasan <reem.mathbout89@yahoo.com>
2020-10-29 11:32:26 +0100

    add routing path

--- currency_converter/src/App.js
+++ currency_converter/src/App.js
@@ -2,18 +2,24 @@ import React, { Component } from "react";
 
 import "./App.css";
 import Converter from "./component/converter";
+import About from "./component/about";
+import CurrenciesList from "./component/currenciesList";
 import NavigatBar from "./component/navBar";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {FormControl,NavDropdown,Navbar,Nav,Form,Button} from 'react-bootstrap'
 class App extends Component {
   render() {
     return (
-      <>
+    <>
     <NavigatBar />
-    <Converter />
-    </>
+    <Router>         
+      <Route exact path="/" component={Converter} />
+      <Route path="/about" component={About} />
+      <Route path="/currenciesList" component={CurrenciesList} />
+    </Router>
+     </>
     );
 
   }
 }
-
 export default App;
