4026cb99c845e82a52ba616b34004b7a3de80539
PierreEpron <pierre.epron@gmail.com>
2020-10-30 11:05:05 +0100

    identation

--- currency_converter/src/component/navBar.js
+++ currency_converter/src/component/navBar.js
@@ -3,23 +3,23 @@ import React, { Component } from "react";
 import {Navbar,Nav} from 'react-bootstrap'
 
 class NavigatBar extends Component {
-render() {
-return (
-<div>
-<Navbar expand="lg" bg="dark" variant="dark">
-    <Navbar.Brand to="/">CurrencyConveter</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/currenciesList">Currencies</Nav.Link>
-        <Nav.Link href="/productCompare">Compare Products</Nav.Link>
-        <Nav.Link href="/about">About</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-</div>
-    );
+  render() {
+    return (
+      <div>
+        <Navbar expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand to="/">CurrencyConveter</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/currenciesList">Currencies</Nav.Link>
+              <Nav.Link href="/productCompare">Compare Products</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+      );
     }
   }
 
